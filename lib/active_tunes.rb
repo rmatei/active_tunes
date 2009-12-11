96c8443f7329490a3617fc79610ffd66de863221
Robert Cezar Matei <rmatei@gmail.com>
2009-12-11 12:02:19 -0800

    require active_mac

--- lib/active_tunes.rb
+++ lib/active_tunes.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-# require 'active_mac'
+require 'active_mac'
 require 'scrobbler'
 Dir["#{File.dirname(__FILE__)}/active_tunes/*.rb"].sort.each { |lib| require lib }
 
